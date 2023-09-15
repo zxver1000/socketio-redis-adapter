2755a2a66d02949bc6e64286ebc0f30855de9cc0
Ko HyeonHo <78923992+zxver1000@users.noreply.github.com>
2023-09-15 16:58:23 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,12 @@ import { ChatGateway } from './chat/chat.gateway';
 import { ChatModule } from './chat/chat.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
-import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [
     //  MongooseModule.forRoot(process.env.MONGO_URL),
     ConfigModule.forRoot({ isGlobal: true }),
     ChatModule,
-    RoomModule,
   ],
   controllers: [AppController],
   providers: [AppService],
